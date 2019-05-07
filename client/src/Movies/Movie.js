107e0c2a6230f25fe4db850a8c0cc66305dac952
anthony <stackchain@gmail.com>
2019-05-07 13:19:40 -0400

    commit - HOOKS version ( w/some errors)

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,58 +1,103 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+// import React, { Component } from "react";
+// import MovieCard from "./MovieCard";
+// import axios from "axios";
+
+// export default class Movie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movie: null
+//     };
+//   }
+
+//   componentDidMount() {
+//     const id = this.props.match.params.id;
+//     this.fetchMovie(id);
+//   }
+
+//   fetchMovie = id => {
+//     axios
+//       .get(`http://localhost:5000/api/movies/${id}`)
+//       .then(response => {
+//         this.setState(() => ({ movie: response.data }));
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   };
+//   // Uncomment this code when you're ready for the stretch problems
+//   componentWillReceiveProps(newProps) {
+//     if (this.props.match.params.id !== newProps.match.params.id) {
+//       this.fetchMovie(newProps.match.params.id);
+//     }
+//   }
+
+//   saveMovie = () => {
+//     console.log(this.props.addToSavedList);
+//     const addToSavedList = this.props.addToSavedList;
+//     addToSavedList(this.state.movie);
+//   };
+
+//   render() {
+//     if (!this.state.movie) {
+//       return <div>Loading movie information...</div>;
+//     }
+
+//     const { title, director, metascore, stars } = this.state.movie;
+//     return (
+//       <div className="save-wrapper">
+//         <MovieCard movie={this.state.movie} />
+
+//         <div className="save-button">
+//           <button onClick={this.saveMovie}>Save</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import axios from "axios";
 
-export default class Movie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
+function Movie(props) {
+  const [movie, setMovie] = useState(null);
 
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    this.fetchMovie(id);
-  }
+  useEffect(() => {
+    const id = props.match.params.id;
+    fetchMovie(id);
+  });
 
-  fetchMovie = id => {
+  function fetchMovie(id) {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
-        this.setState(() => ({ movie: response.data }));
-      })
+      .then(response => setMovie(response.data))
       .catch(error => {
         console.error(error);
       });
-  };
-  // Uncomment this code when you're ready for the stretch problems
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
-    }
   }
 
-  saveMovie = () => {
-    console.log(this.props.addToSavedList);
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
-  };
+  function saveMovie() {
+    console.log(props.addToSavedList);
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  }
 
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
+  if (!movie) {
+    return <div>Loading movie information...</div>;
+  }
 
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
+  const { title, director, metascore, stars } = movie;
 
-        <div className="save-button">
-          <button onClick={this.saveMovie}>Save</button>
-        </div>
+  return (
+    <div className="save-wrapper">
+      <MovieCard movie={movie} />
+
+      <div className="save-button">
+        <button onClick={saveMovie}>Save</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Movie;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,36 +1,66 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
 import axios from "axios";
 
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+function MovieList() {
+  const [movies, setMovies] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
+      .then(response => setMovies(response.data))
       .catch(error => {
         console.error("Server Error", error);
       });
-  }
+  });
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
-            <MovieCard key={movie.id} movie={movie} />
-          </Link>
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+          <MovieCard key={movie.id} movie={movie} />
+        </Link>
+      ))}
+    </div>
+  );
 }
+
+export default MovieList;
+
+// import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import MovieCard from "./MovieCard";
+// import axios from "axios";
+
+// export default class MovieList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movies: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     axios
+//       .get("http://localhost:5000/api/movies")
+//       .then(response => {
+//         this.setState(() => ({ movies: response.data }));
+//       })
+//       .catch(error => {
+//         console.error("Server Error", error);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div className="movie-list">
+//         {this.state.movies.map(movie => (
+//           <Link to={`/movies/${movie.id}`}>
+//             <MovieCard key={movie.id} movie={movie} />
+//           </Link>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
